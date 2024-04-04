3a90c91c40deec484a5b908d87cbd66efe9895c4
rdtyaaa <yogaradit123@gmail.com>
2024-04-04 09:06:09 +0700

    Homepage

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -140,7 +140,7 @@
                             <path
                                 d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
                         </svg>
-                        <span class="ms-3">Dashboard</span>
+                        <span class="ms-3">Home</span>
                     </a>
                 </li>
                 <li>
@@ -152,12 +152,12 @@
                             <path
                                 d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z" />
                         </svg>
-                        <span class="ms-3 flex-1 whitespace-nowrap">Kanban</span>
+                        <span class="ms-3 flex-1 whitespace-nowrap">Search</span>
                         <span
                             class="ms-3 inline-flex items-center justify-center rounded-full bg-gray-100 px-2 text-sm font-medium text-gray-800 dark:bg-gray-700 dark:text-gray-300">Pro</span>
                     </a>
                 </li>
-                <li>
+                {{-- <li>
                     <a href="#"
                         class="group flex items-center rounded-lg p-2 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
                         <svg class="h-5 w-5 flex-shrink-0 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
@@ -222,14 +222,14 @@
                         </svg>
                         <span class="ms-3 flex-1 whitespace-nowrap">Sign Up</span>
                     </a>
-                </li>
+                </li> --}}
             </ul>
         </div>
     </aside>
 
     <div class="flex h-screen flex-col sm:ml-64">
         <div
-            class="scrollbar-thumb-rounded-full scrollbar scrollbar-thin scrollbar-thumb-gray-100 mt-16 h-32 flex-1 overflow-y-auto overflow-y-scroll rounded-tl-lg bg-gray-100 p-4">
+            class="scrollbar-thumb-rounded-full scrollbar scrollbar-thin scrollbar-thumb-gray-100 mt-16 flex h-32 flex-1 overflow-y-auto overflow-y-scroll rounded-tl-lg bg-gray-100 p-4">
             @yield('content')
         </div>
     </div>
