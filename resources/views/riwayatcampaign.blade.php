5a30a9f845097275aab562b1fc4e92a70d4536df
ninyomanamerta <kusumayanti114@gmail.com>
2024-04-04 10:00:24 +0700

    RPL450103-11

---
+++ resources/views/riwayatcampaign.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.master')
+
+@section('content')
+
+
+    <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-screen-md mx-10 hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700" style="max-width: 66rem; margin: 0 auto; position: relative;">
+        <img class="object-cover w-full max-h-10 rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="{{ asset('assets/img/EduFund.png') }}" alt="" style="width: 100px; height: auto; position: absolute; top: 0; left: 0;">
+        <div class="flex flex-col justify-between p-4 leading-normal">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+        </div>
+    </a>
+
+    <br>
+
+    <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-screen-md mx-10 hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700" style="max-width: 66rem; margin: 0 auto; position: relative;">
+        <img class="object-cover w-full max-h-10 rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="{{ asset('assets/img/EduFund.png') }}" alt="" style="width: 100px; height: auto; position: absolute; top: 0; left: 0;">
+        <div class="flex flex-col justify-between p-4 leading-normal">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+        </div>
+    </a>
+
+
+@endsection
