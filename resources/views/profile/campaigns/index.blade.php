128978e30288ff71f1280b7237bef34917f4fbe6
dhafinfauzan <prdhafin@gmail.com>
2024-04-17 10:05:12 -0700

    commit10

--- resources/views/profile/campaigns/index.blade.php
+++ resources/views/profile/campaigns/index.blade.php
@@ -4,7 +4,10 @@
     <h1>Campaigns</h1>
     <ul>
         @foreach($campaigns as $campaign)
-            <li>{{ $campaign->name }} - {{ $campaign->description }}</li>
+            <li>
+                <strong>{{ $campaign->name }}</strong> - {{ $campaign->description }}
+                <br>Target Donasi : ${{ number_format($campaign->target_donation, 2) }}
+            </li>
         @endforeach
     </ul>
 @endsection
