9f6aa0c2eef7bbb3da62d79ead42b26025e0b60b
dhafinfauzan <prdhafin@gmail.com>
2024-04-17 08:57:43 -0700

    commit4

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CampaignController;
 
 Route::get('/', function () {
-    return view('buatcampaign');
+    return view('index');
 });
 
 Route::get('/campaigns/create', [CampaignController::class, 'create'])->name('campaigns.create');
@@ -16,26 +15,3 @@ Route::post('/campaigns', [CampaignController::class, 'store'])->name('campaigns
 Route::get('/daftar-campaigns', [CampaignController::class, 'index'])->name('daftar');
 
 
-Route::get('/home', function () {
-    return view('index');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
-
-Route::get('/riwayat', function () {
-    return view('riwayatcampaign');
-});
-
-Route::get('/riwayat/donatur', function () {
-    return view('lihatdonatur');
-});
-
-Route::get('/verifikasi-sekolah', function () {
-    return view('verifikasi-sekolah');
-});
