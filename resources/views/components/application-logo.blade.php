df8445defa184dde75ce839ff637aeb724c6ea31
sofiyatunhasanah <116246577+sofiyatunhasanah@users.noreply.github.com>
2024-04-17 21:15:56 +0700

    application-logo

---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,1 @@
+<img src="{{ asset('img/EduFund2.png') }}" class="me-3 h-10" alt="EduFund" />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-<<<<<<<<< Temporary merge branch 1
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 
